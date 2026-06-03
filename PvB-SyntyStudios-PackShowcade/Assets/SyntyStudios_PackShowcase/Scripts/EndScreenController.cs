6698328cfc7e68810cd17da033bcc903d2194bf1
Elger Creutzberg <elger.creutzberg@gmail.com>
2026-06-03 10:27:48 +0200

    Final touches

--- PvB-SyntyStudios-PackShowcade/Assets/SyntyStudios_PackShowcase/Scripts/EndScreenController.cs
+++ PvB-SyntyStudios-PackShowcade/Assets/SyntyStudios_PackShowcase/Scripts/EndScreenController.cs
@@ -1,15 +1,42 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class EndScreenController : MonoBehaviour
 {
+    [SerializeField] private BlackBoxesController blackBoxesController;
+    [SerializeField] private GameObject ui;
+
+    private void Awake()
+    {
+        blackBoxesController.Close(instant: true);
+        ui.SetActive(false);
+    }
+
+    private void Start()
+    {
+        StartSequence().Forget(); 
+    } 
+
+    private async UniTask StartSequence()
+    {
+        await blackBoxesController.Open();
+        ui.SetActive(true);
+    }
+
     public void Home()
     {
-        SceneManager.LoadScene("StartScreen");
+        HomeSequence().Forget();
     }
 
     public void CloseGame()
     {
         Application.Quit();
+    }
+
+    private async UniTask HomeSequence()
+    {
+        await blackBoxesController.Close();
+        SceneManager.LoadScene("StartScreen");
     }
 }
